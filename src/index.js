b8fdb10dca1ff8c71fe91767ed0dff747a4e64da
jarodares <jrob.valladares@gmail.com>
2019-02-16 22:24:53 -0600

    DB config added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const config = require('./server/config');
-
+//start database
+require('./database')
+//start server
 const app = config(express());
 
 app.listen(app.get('port'), () => {
